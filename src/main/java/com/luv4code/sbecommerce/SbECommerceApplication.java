ece286476399b8a249f25ead8b155aa01619b6cc
madhav-codehub <madhav.p.akv@gmail.com>
2026-03-18 16:26:18 +0530

    e-commerce app basic setup

---
+++ src/main/java/com/luv4code/sbecommerce/SbECommerceApplication.java
@@ -0,0 +1,13 @@
+package com.luv4code.sbecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbECommerceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SbECommerceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/luv4code/sbecommerce/SbECommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luv4code.sbecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbECommerceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
